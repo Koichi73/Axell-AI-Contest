559c64e8850ddcdc141a75f859e9effe0b316bc8
Koichi73 <koichiozaki0@gmail.com>
2024-08-22 18:51:00 +0900

    Update: delete other models

--- src/utils/models.py
+++ src/utils/models.py
@@ -4,48 +4,7 @@
 # また、出力も同様のフォーマットで、縦横の解像度(H, W)が4倍となります。
 
 import torch
-from torch import nn, tensor, clip
-
-class ESPCN4x(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.scale = 4
-        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding=2)
-        nn.init.normal_(self.conv_1.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_1.bias)
-
-        self.act = nn.ReLU()
-
-        self.conv_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_2.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_2.bias)
-        
-        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_3.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_3.bias)
-
-        self.conv_3_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_3_2.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_3_2.bias)
-
-        self.conv_4 = nn.Conv2d(in_channels=32, out_channels=(1 * self.scale * self.scale), kernel_size=3, padding=1)
-        nn.init.normal_(self.conv_4.weight, mean=0, std=0.001)
-        nn.init.zeros_(self.conv_4.bias)
-
-        self.pixel_shuffle = nn.PixelShuffle(self.scale//2)
-
-    def forward(self, X_in: tensor) -> tensor:
-        X = X_in.reshape(-1, 1, X_in.shape[-2], X_in.shape[-1])
-        X = self.act(self.conv_1(X))
-        X = self.act(self.conv_2(X))
-        X = self.act(self.conv_3(X))
-        X = self.act(self.conv_3_2(X))
-        X = self.conv_4(X)
-        X = self.pixel_shuffle(X)
-        X = self.pixel_shuffle(X)
-        X = X.reshape(-1, 3, X.shape[-2], X.shape[-1])
-        X_out = clip(X, 0.0, 1.0)
-        return X_out
+from torch import nn
 
 class ResidualBlock(nn.Module):
     def __init__(self, num_channels):
@@ -100,26 +59,6 @@ class EDSR(nn.Module):
         x = self.pixel_shuffle(x)
         x = self.pixel_shuffle(x)
         x_out = torch.clamp(x, 0.0, 1.0)
-        return x_out
-    
-class VDSR(nn.Module):
-    def __init__(self):
-        super(VDSR, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv_layers = nn.Sequential(
-            *[nn.Sequential(nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True)) for _ in range(8)]
-        )
-        self.conv_last = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
-    
-    def forward(self, x):
-        x = nn.functional.interpolate(x, scale_factor=4, mode='bicubic', align_corners=False)
-        residual = x
-        out = self.relu(self.conv1(x))
-        out = self.conv_layers(out)
-        out = self.conv_last(out)
-        out += residual # 残差を加算
-        x_out = torch.clamp(out, 0.0, 1.0)
         return x_out
 
 if __name__ == "__main__":
